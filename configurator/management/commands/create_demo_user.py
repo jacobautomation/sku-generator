519aade43cb6aaa036be085576686226aff7c727
Jacob <excel.jacob@outlook.dk>
2026-09-09 07:22:23 +0200

    Support custom username/password for demo logins

--- configurator/management/commands/create_demo_user.py
+++ configurator/management/commands/create_demo_user.py
@@ -3,20 +3,27 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = "Create/reset the demo login for David (username: david, password: 12345)."
+    help = "Create/reset a demo login. Usage: python manage.py create_demo_user [username] [password]"
+
+    def add_arguments(self, parser):
+        parser.add_argument("username", nargs="?", default="david")
+        parser.add_argument("password", nargs="?", default="12345")
 
     def handle(self, *args, **options):
+        username = options["username"]
+        password = options["password"]
+
         User = get_user_model()
         user, created = User.objects.get_or_create(
-            username="david",
+            username=username,
             defaults={"is_staff": True, "is_superuser": True},
         )
         user.is_staff = True
         user.is_superuser = True
-        user.set_password("12345")
+        user.set_password(password)
         user.save()
 
         action = "Created" if created else "Updated"
         self.stdout.write(self.style.SUCCESS(
-            f"{action} user 'david' with password '12345' (staff + admin access)."
+            f"{action} user '{username}' with password '{password}' (staff + admin access)."
         ))
